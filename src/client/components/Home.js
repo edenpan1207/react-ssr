1fb3455a42af207a4ba526b04542986ba6b398c2
y_eden <xadam69@ec-mart.com>
2020-10-06 09:57:05 +0800

    [add] ssr package and user component

---
+++ src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <div>Home This Page</div>
+      <button onClick={() => console.log('press me!!!')}>press me</button>
+    </div>
+  )
+}
+
+export default Home;
